65ebc75cb914cbc3781eda621872052d0afbb3fd
Christian Perfect <christianperfect@gmail.com>
2016-05-20 09:00:47 -0700

    improve the PDF regex

--- parse-bib.php
+++ parse-bib.php
@@ -168,7 +168,7 @@ class BibEntry {
 
 	function pdf() {
 		foreach($this->urls as $url) {
-			if(preg_match('#(?:\.pdf$)|(?:arxiv\.org/pdf)#',$url)) {
+			if(preg_match('#(?:\.pdf(?:\?.*)?$)|(?:arxiv\.org/pdf)#',$url)) {
 				return $url;
 			}
 		}
