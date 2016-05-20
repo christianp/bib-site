36e3ef280a51480891a1d0e09ffebd6ec0551521
Christian Perfect <christianperfect@gmail.com>
2016-05-20 03:32:20 -0700

    redirect to entry page when there's only one result

--- views/index.php
+++ views/index.php
@@ -3,6 +3,8 @@ global $BIB;
 
 $entries = $BIB->db->records;
 
+$sort = $_GET['sort'] ? $_GET['sort'] : 'title';
+
 global $words;
 $query = strtolower($_GET['q']);
 $words = explode(" ",$query);
@@ -17,16 +19,31 @@ if($query) {
 		}
 		return true;
 	});
+	if(count($entries)==1) {
+		$entry = reset($entries);
+		redirect(reverse('view_entry',array('key'=>$entry->key)));
+	}
 }
 
-usort($entries,function($a,$b) {
-	$a = strtolower($a->title);
-	$b = strtolower($b->title);
-	if($a==$b) {
-		return 0;
-	}
-	return $a>$b ? 1 : -1;
-});
+if($sort=='title') {
+	usort($entries,function($a,$b) {
+		$a = strtolower($a->title);
+		$b = strtolower($b->title);
+		if($a==$b) {
+			return 0;
+		}
+		return $a>$b ? 1 : -1;
+	});
+} else if($sort=='date') {
+	usort($entries,function($a,$b) {
+		$a = strtolower($a->fields['urldate']);
+		$b = strtolower($b->fields['urldate']);
+		if($a==$b) {
+			return 0;
+		}
+		return $a>$b ? -1 : 1;
+	});
+}
 
 echo $BIB->twig->render('index.html',array(
 	'entries'=>$entries,
